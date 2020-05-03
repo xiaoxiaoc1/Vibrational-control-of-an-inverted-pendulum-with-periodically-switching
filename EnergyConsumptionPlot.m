40cab475a75612899bda2617371cd467ad86b1c1
Xiaoxiao Cheng <42459673+xiaoxiaoc1@users.noreply.github.com>
2020-05-03 16:13:44 +0100

    Update EnergyConsumptionPlot.m

--- EnergyConsumptionPlot.m
+++ EnergyConsumptionPlot.m
@@ -1,7 +1,7 @@
 figure();
-load('matlab_Tu0');
+load('matlab_w0Tu0');
 E1 = EnergyConsumption(t,x,sigma);
-load('matlab_Tu02');
+load('matlab_w1Tu02');
 E2 = EnergyConsumption(t,x,sigma);
 plot(t,E1,t,E2);
 legend('T_u =0','T_u = 0.2');
